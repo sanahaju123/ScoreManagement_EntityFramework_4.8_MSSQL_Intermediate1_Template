5af235fb7bdea56c89abf7f544c6d4be9d53f1bc
sanahaju123 <88725494+sanahaju123@users.noreply.github.com>
2023-11-27 14:01:36 +0530

    Update 202311051518228_initial.cs

--- ScoreMangementApp/Migrations/202311051518228_initial.cs
+++ ScoreMangementApp/Migrations/202311051518228_initial.cs
@@ -1,29 +1,30 @@
-﻿namespace ScoreManagementTracker.Migrations
+namespace ScoreManagementApp.Migrations
 {
     using System;
     using System.Data.Entity.Migrations;
 
-    public partial class InitialScoreEntry : DbMigration
+    public partial class initial : DbMigration
     {
         public override void Up()
         {
             CreateTable(
-                "dbo.ScoreEntries",
+                "dbo.Scores",
                 c => new
-                {
-                    Id = c.Int(nullable: false, identity: true),
-                    Date = c.DateTime(nullable: false),
-                    PlayerName = c.String(),
-                    PlayerScore = c.Int(nullable: false),
-                    GameType = c.String(),
-                    Notes = c.String(),
-                })
-    .PrimaryKey(t => t.Id);
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Date = c.DateTime(nullable: false),
+                        PlayerName = c.String(),
+                        PlayerScore = c.Int(nullable: false),
+                        GameType = c.String(),
+                        Notes = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+
         }
 
         public override void Down()
         {
-            DropTable("dbo.ScoreEntries");
+            DropTable("dbo.Scores");
         }
     }
 }
